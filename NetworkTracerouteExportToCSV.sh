3e4ca652f516d1449396d4cfe5b784a7a67f0d84
Guillermo Alba <35967127+themems@users.noreply.github.com>
2019-07-18 11:11:30 -0500

    Update NetworkTracerouteExportToCSV.sh

--- NetworkTracerouteExportToCSV.sh
+++ NetworkTracerouteExportToCSV.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 for i in $(seq 0 255); do
  for j in $(seq 0 255); do
- fping -c 1 -u 10.24.$i.$j >& /dev/null
-  if [ $? -ne 0 ];
-  then echo 10.24.$i.$j
-  else
-   mtr --csv -c 1 10.24.$i.$j > 10.24.$i.$j.csv
-   cat 10.* >  resumen.csv
-   rm 10.*
-  fi
-  done
+  for k in $(seq 0 255); do
+   fping -c 1 -u 10.$i.$j.$k >& /dev/null
+   if [ $? -ne 0 ];
+   then echo 10.$i.$j.$k
+   else
+    mtr --csv -c 1 10.$i.$j.$k > 10.$i.$j.$k.csv
+    cat 10.* >  resumen.csv
+   fi
+   done
+ done
 done
