7ddf6831ef665f930cef80203555b67fcca5a69b
Guillermo Alba <35967127+themems@users.noreply.github.com>
2020-04-24 13:55:33 -0500

    01

--- TelegramWebServerWithZabbix.sh
+++ TelegramWebServerWithZabbix.sh
@@ -6,6 +6,6 @@ LOGFILE="/var/log/zabbix/telegram.log"		#Crea y define el archivo de log
 to=${to// /+}								#
 subject=${subject// /+}						#Elimina los espacios vacios del texto y los sustituye por "+" para dar formato para el envio.
 body=${body// /+}							#
-curl --data "Msg="$body"&ChatId="$to"&BotToken="298111298:AAGp-gWWhrHPAzUkIYq7QaNYLM7hCqdRuJs"" http://10.24.1.26/WSTelegram/WSTelegram.asmx/EnviaTLMsg >> ${LOGFILE}		#Envia mensaje
+curl --data "Msg="$body"&ChatId="$to"&BotToken="<TelegramBotToken>"" http://10.24.1.26/WSTelegram/WSTelegram.asmx/EnviaTLMsg >> ${LOGFILE}		#Envia mensaje
 echo "Recipent=$to Message=$subject Body=$body" >> ${LOGFILE}		#Escribe resultado en log 
 echo "Finished" >> ${LOGFILE}
